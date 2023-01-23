3505e13b537fbcc7a8f060023edf6366d26a843c
bbakjun <wnsguddl789@gmail.com>
2023-01-23 19:34:31 +0900

    feat(todo/api) : 폴더구조 변경 및 파일 삭제

--- src/hooks/useRouter.ts
+++
@@ -1,9 +0,0 @@
-import { useNavigate, useLocation, useParams } from 'react-router-dom';
-
-export default function useRouter() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const params = useParams();
-
-  return { location, params, navigate };
-}
--- src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthProps } from '../types';
-
-import { withAuth } from '@helper';
-
-interface Props extends AuthProps {}
-
-export const HomePage = withAuth(({}: Props) => {
-  return <div>홈</div>;
-});
